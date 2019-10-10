72749e22495fcf822116bdb251865f9d7be994e4
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-10 11:32:24 -0600

    Add react-router and navlinks to all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import Home from './Home';
+import Unicorn from './Unicorn';
+import Shark from './Shark';
+import Puppies from './Puppy';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <main className="App">
-        Hello World!
+        <header>
+          <NavLink exact to='/' className='nav'>Home</NavLink>
+          <NavLink exact to='/unicorns' className='nav'>Unicorns</NavLink>
+          <NavLink exact to='/puppies' className='nav'>Puppies</NavLink>
+          <NavLink exact to='/sharks' className='nav'>Sharks</NavLink>
+        </header>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/unicorns' component={Unicorn} />
+        <Route exact path='/sharks' component={Shark} />
+        <Route exact path='/puppies' component={Puppies} />
       </main>
     );
   }
